64eff6b766a6bb1d1433c86acad4b04ea48fc2f9
zerolo <ze.p.rolo@gmail.com>
2024-05-27 11:00:37 +0100

    version update

--- custom_components/aguasgaia/__init__.py
+++ custom_components/aguasgaia/__init__.py
@@ -7,7 +7,7 @@ from homeassistant.core import HomeAssistant
 from homeassistant.helpers.aiohttp_client import async_get_clientsession
 from homeassistant.helpers.typing import ConfigType
 
-__version__ = "0.0.3"
+__version__ = "0.0.6"
 _LOGGER = logging.getLogger(__name__)
 _LOGGER.setLevel(logging.DEBUG)
 
